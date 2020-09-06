76c799fef7c8f71d23d638e1f395a49160e86214
Issam <issam.git@issam.ma>
2020-09-06 10:58:57 +0100

    Delete Datascientist association website

--- web/deploy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-rsync -r /home/issam/git/moroccan.datascientist/web/* deploy@vps:/var/www/html/moroccan.datascientist/
-
--- web/js/md.js
+++
@@ -1,8 +0,0 @@
-$('document').ready(function(){
-    var lang = 'fr';
-    $.getJSON("content/translations.json", function(json) {
-        Object.keys(json).forEach(function(key) {
-            $(".i18n-"+key).text(json[key][lang]);
-          })
-    });
-});
